8856b9ab8aef30a0bea8417638bea83ba47ecf31
mukhsia <mukhsia@pdx.edu>
2019-05-15 18:42:04 -0700

    Initial stuff, still need to figure out dropdown nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">CS565-Final-Project</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">PSU-PUBKEYCRYPT</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">PSU-CRYPT</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Battlecode 2019 Bot</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#about me">About Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>;
+
         <h1>Hello World!</h1>
       </header>
     </div>
